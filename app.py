83b90a3056a7abc77e55832a6c7167537480ade1
me <famlinkus@gmail.com>
2018-07-18 15:59:40 +0530

    update

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+import json
+import re
+
 import requests
 from flask import (Flask, Response, make_response, redirect, render_template,
                    request, send_file, session)
 from htmlmin.minify import html_minify
-import ytsearch
+
 app = Flask(__name__)
-USER_AGENT = "Mozilla/5.0 (Windows; U; Windows NT 10.0; en-US)\
+ua = "Mozilla/5.0 (Windows; U; Windows NT 10.0; en-US)\
  AppleWebKit/604.1.38 (KHTML, like Gecko) Chrome/68.0.3325.162"
 app.secret_key = "7bf9a280"
+basic_headers = {
+    "User-Agent": ua,
+    "Upgrade-Insecure-Requests": "1",
+    "dnt": '1',
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+}
 
 
 @app.route("/", strict_slashes=False)
@@ -38,8 +47,15 @@ def search():
 @app.route("/search/fetch/", methods=['POST'])
 def search_json():
     q = request.form.get("q")
-    data = ytsearch.yt_search(q)
-    return Response(data, content_type="application/json")
+    if re.sub(r"\s", "", q):  # has some text
+        trending = False
+        req = "http://youtube.com/results?search_query="+q
+    else:
+        trending = True
+        req = "http://youtube.com/feed/trending/"
+    print(req)
+    htm = requests.get(req, headers=basic_headers).text
+    return Response(json.dumps({"html": htm, "trending": trending}), content_type="application/json")
 
 
 if __name__ == '__main__':
