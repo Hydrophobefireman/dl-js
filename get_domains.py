42ba440bab4d5c401c9833844e211d43c4a0490c
HydroPhobeFireman <bhavesh.pareek36@gmail.com>
2019-10-27 14:43:09 +0530

    fix domains

--- get_domains.py
+++ get_domains.py
@@ -1,11 +1,4 @@
-openload = r"""
-                    (?:
-                        openload\.(?:co|io|link|pw)|
-                        oload\.(?:tv|best|biz|stream|site|xyz|win|download|cloud|cc|icu|fun|club|info|online|press|pw|life|live|space|services|website|vip)|
-                        oload\.(?:tv|best|biz|stream|site|xyz|win|download|cloud|cc|icu|fun|club|info|online|monster|press|pw|life|live|space|services|website|vip)|
-                        oladblock\.(?:services|xyz|me)|openloed\.co
-                    )
-                """
+openload = r"""(?:openload\.(?:co|io|link|pw)|oload\.(?:tv|best|biz|stream|site|xyz|win|download|cloud|cc|icu|fun|club|info|online|press|pw|life|live|space|services|website|vip)|oload\.(?:tv|best|biz|stream|site|xyz|win|download|cloud|cc|icu|fun|club|info|online|monster|press|pw|life|live|space|services|website|vip)|oladblock\.(?:services|xyz|me)|openloed\.co)"""
 
 keeload = r"""keeload"""
 
